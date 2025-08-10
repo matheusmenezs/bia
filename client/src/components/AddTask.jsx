a4ffc0426e13a57ba9ecc649d2679cb37506084e
Matheus Menezes <menezes.matheush@gmail.com>
2025-08-10 04:38:47 +0000

    alter button

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefa
+        Add new Task
       </button>
       
       <Modal
